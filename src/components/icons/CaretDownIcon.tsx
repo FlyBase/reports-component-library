ac694327ac18a5facdae9c4dc1f8cb19ebc3d625
Seth Campbell <sercampb@iu.edu>
2024-08-26 10:50:59 -0400

    injector for gal4 search page

---
+++ src/components/icons/CaretDownIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import {ClickableProps} from "../../types/clickableProps";
+
+const CaretDown: React.FC<ClickableProps> = ({ onClick }) => (
+
+);
+export default CaretDown;
---
+++ src/injectors/injectGal4Search.tsx
@@ -0,0 +1,25 @@
+import injectComponents from "./injectComponents";
+import GOSummaryRibbonsSection from "../sections/GOSummaryRibbonsSection";
+import ExpressionSummaryRibbonsSection from "../sections/ExpressionSummaryRibbonsSection";
+import DiseaseModelSummaryRibbonSection from "../sections/DiseaseModelSummaryRibbonSection";
+import GeneToolKitMostCommonlyUsedSection from "../sections/GeneToolKitMostCommonlyUsedSection";
+
+/*
+* This file is a webpack entry point, and will get built separately when running `yarn build`
+* */
+
+//load a gene report with all react code
+const injectGeneReport = () => {
+    injectComponents([
+        { containerId: "goSummaryRibbonSectionContainer", component: <GOSummaryRibbonsSection reportId={FBgn} /> },
+        { containerId: "expressionSummaryRibbonsSectionContainer", component: <ExpressionSummaryRibbonsSection reportId={FBgn} /> },
+        { containerId: "diseaseModelSummaryRibbonSectionContainer", component: <DiseaseModelSummaryRibbonSection reportId={FBgn} /> },
+        { containerId: "geneToolkitSectionContainer", component: <GeneToolKitMostCommonlyUsedSection /> },
+    ]);
+};
+
+//only do this automatically in production (when build version is being used)
+//this makes it so that including the build file on the page automatically handles everything, without extra code
+if(process.env.NODE_ENV === "production") {
+    injectGeneReport();
+}
